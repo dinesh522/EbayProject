66acaf099590c405cb7005dc53f7885179a484aa
dinesh522 <53029030+dinesh522@users.noreply.github.com>
2019-07-22 22:19:43 +1000

    update

--- EbayBDDFrameWork/src/main/java/Runner/TestRunner.java
+++ EbayBDDFrameWork/src/main/java/Runner/TestRunner.java
@@ -10,7 +10,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\acer\\eclipse-workspace\\EbayBDDFrameWork\\src\\main\\java\\Features\\AddToCart.feature", glue = {
+@CucumberOptions(features = "src\\main\\java\\Features\\AddToCart.feature", glue = {
 		"stepDefinition" }, plugin = { "pretty", "html:target/cucumber-html-report",
 				"com.cucumber.listener.ExtentCucumberFormatter:C:\\Users\\acer\\git\\EbayProject\\EbayBDDFrameWork\\Report\\report.html" }, monochrome = true, strict = true, dryRun = false)
 
